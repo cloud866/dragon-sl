4667dfe54edb5a82f3a97e4d9112732ff1926d4c
cloud866 <dklein927@gmail.com>
2023-04-05 16:54:12 +0300

    Initial commit

---
+++ dragon-sl/src/domain/monsters/lvl1/Skeleton.java
@@ -0,0 +1,9 @@
+package domain.monsters.lvl1;
+
+import entity.FantasyCharacter;
+
+public class Skeleton extends FantasyCharacter {
+    public Skeleton(String name, int healthPoints, int strength, int dexterity, int xp, int gold) {
+        super(name, healthPoints, strength, dexterity, xp, gold);
+    }
+}
---
+++ dragon-sl/src/domain/npc/Merchant.java
@@ -0,0 +1,19 @@
+package domain.npc;
+
+import entity.Seller;
+
+public class Merchant implements Seller {
+    //Метод для продажи
+    @Override
+    public String sell(Goods goods) {
+        String result = "";
+        if (goods == Goods.POTION) {
+            result = "potion";
+        }
+        return result;
+    }
+    //Энам для товаров
+    public enum Goods {
+        POTION
+    }
+}
